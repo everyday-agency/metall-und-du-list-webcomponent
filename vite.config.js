4fbdfd95ecf103f305fb1d31d08c416078b780de
Benedikt <info@benediktgrether.de>
2025-04-03 10:41:28 +0200

    Update folder structure and add new base url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: "/web-componet-demo/",
-	build: {
-		outDir: "dist", // Ensure this is set correctly
-	},
+    base: '/metall-und-du-list-webcomponent/',
+    build: {
+        outDir: 'dist', // Ensure this is set correctly
+    },
 });
