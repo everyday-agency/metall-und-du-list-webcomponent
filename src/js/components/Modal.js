d0a03eb373e7be445919a91ebac85a6bdcd7be3e
Benedikt <info@benediktgrether.de>
2025-04-04 11:54:27 +0200

    Add url and flex items

--- src/js/components/Modal.js
+++ src/js/components/Modal.js
@@ -2,18 +2,30 @@ export function renderModal(item) {
     const template = `
         <div class="modal fixed inset-0 z-50 bg-black/50 flex items-center justify-center">
             <div class="bg-white p-6 rounded shadow-lg w-3/4 relative">
-                <button class="close-modal absolute top-2 right-2 text-gray-600 text-xl">×</button>
-                <div>
-                <p class="text-sm">${item.professionNameDeMf}</p>
-                <h2 class="font-bold text-2xl">${item.locationName}</h2>
-                <p>${item.locationStreet} ${item.locationHouseNumber}</p>
-                <p>${item.locationZipCode} ${item.locationLocalityNameDe}</p>
+                <button class="close-modal absolute top-2 right-2 text-gray-600 text-3xl pr-3">×</button>
+                <div class="flex flex-col lg:flex-row gap-4 justify-between items-center p-6">
+                    <div>
+                        <p class="text-sm">${item.professionNameDeMf}</p>
+                        <h2 class="font-bold text-2xl">${item.locationName}</h2>
+                        <p>${item.locationStreet} ${
+        item.locationHouseNumber
+    }</p>
+                        <p>${item.locationZipCode} 
+                        ${item.locationLocalityNameDe}</p>
+                    <div class="mt-4">
+                        <h3 class="font-bold">Kontaktdaten Bewerbung</h3>
+                        <p>${renderContactName(item)}</p>
+                        <p>${renderEmail(item)}</p>
+                        <p>${renderPhone(item)}</p>
+                    </div>
                 </div>
-                <div class="mt-4">
-                    <h3 class="font-bold">Kontaktdaten Bewerbung</h3>
-                    <p>${renderContactName(item)}</p>
-                    <p>${renderEmail(item)}</p>
-                    <p>${renderPhone(item)}</p>
+                <div>
+                    <ul class="flex flex-col lg:items-end">
+                        ${renderAdditionalInfromation(item)}
+                    </ul>
+                    <div class="mt-4">                        
+                        ${renderURL(item)}
+                    </div>
                 </div>
             </div>
         </div>
@@ -97,4 +109,29 @@ function formatPhoneNumber(phoneNumber) {
     );
 
     return { href, formatted };
+}
+
+function renderAdditionalInfromation(item) {
+    const additionalInfoHTML = `<li class="flex items-center gap-2">Offene Lehrstelle 2025 ${
+        item.apprenticeshipPlaceSchoolYears
+            ? '<span class="flex bg-green-500 w-[1rem] h-[1rem] rounded-full"></span>'
+            : '<span class="flex bg-red-500 w-[1rem] h-[1rem] rounded-full"></span>'
+    }</li>
+            <li class="flex items-center gap-2">Schnupperlehre auf Anfrage möglich <span class="flex bg-green-500 w-[1rem] h-[1rem] rounded-full"></span> </li>
+            `;
+    return additionalInfoHTML;
+}
+
+function renderURL(item) {
+    const url =
+        item.apprenticeshipPlaceSchoolYears?.[0]?.url ||
+        item.applicationUrl ||
+        item.locationUrl;
+
+    const urlHTML = url
+        ? `<h3 class="font-bold">Mehr über das Unternehmen erfahren</h3>
+        <a href="${url}" class="text-black underline">${item.locationName}</a>`
+        : '';
+
+    return urlHTML;
 }
